4f0f7b4999a009b229aafab85de61cebec34fc12
skillzl <user@skillzl.me>
2021-12-26 01:19:11 +0200

    xmas update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import styles from "./App.module.scss";
 
+// start xmas stuff
+import Lightrope from "./components/Lightrope";
+import Snowfall from "react-snowfall";
+// end xmas stuff
+
 import * as React from "react";
 
 import { Footer, Header, Todos } from "./components";
@@ -7,6 +12,8 @@ import { Footer, Header, Todos } from "./components";
 export default function App() {
   return (
     <div className={styles.container}>
+      <Lightrope />
+      <Snowfall />
       <Header />
       <Todos />
       <Footer />
---
+++ src/components/Lightrope.js
@@ -0,0 +1,50 @@
+/** @format */
+
+import "./Lightrope.scss";
+
+import * as React from "react";
+
+export default function Lightrope() {
+  return (
+    <ul className="lightrope">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+}
